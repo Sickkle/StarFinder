12c5fb33f45f3f5949ad8f26be8bc33942712da5
JasonPlayzs20 <103009819+JasonPlayzs20@users.noreply.github.com>
2024-11-16 15:10:10 -0500

    Added stars that are independently globally aware of their position

--- Assets/SkySpherePlacer.cs
+++ Assets/SkySpherePlacer.cs
@@ -4,12 +4,31 @@ using UnityEngine;
 
 public class SkySpherePlacer : MonoBehaviour
 {
+    public GameObject spherePrefab;
+    public int numOfSpherees = 10;
+    public Vector3[] fixedPositions;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (fixedPositions.Length == 0)
+        {
+            fixedPositions = new Vector3[numOfSpherees];
+            for (int i = 0; i < numOfSpherees; i++)
+            {
+                fixedPositions[i] = new Vector3(
+                    Random.Range(-10f, 10f),
+                    Random.Range(5f, 15f),
+                    Random.Range(-10f, 10)
+                    );
+            
+            }
+        }
+        foreach (Vector3 position in fixedPositions)
+        {
+            Instantiate(spherePrefab, position, Quaternion.identity);
+        }
     }
-
     // Update is called once per frame
     void Update()
     {
